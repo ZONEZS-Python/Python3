7d814701793be1715df420eeebc33b77de9eccbd
zonezs <zonezs123@gmail.com>
2020-05-11 18:39:27 +0800

    add post service for student write infomation

--- FastAPI/TestAPI/main.py
+++ FastAPI/TestAPI/main.py
@@ -1,17 +1,28 @@
-
 '''
 @Author: _zone
 @Date: 2020-05-11 15:52:00
-@LastEditTime: 2020-05-11 17:17:51
+@LastEditTime: 2020-05-11 18:37:47
 @FilePath: /FastAPIDemo/TestAPI/main.py
 '''
 
 
 from fastapi import FastAPI
-# import json
+from pydantic import BaseModel
 import uvicorn
 
+
 app = FastAPI()
+
+
+# 学生用户信息 model
+class StudentModel(BaseModel):
+    name: str
+    student_id: int
+    age: int
+    height: int
+    weight: int
+    sex: str
+
 
 name = "Jack Ma"
 student_id = 15703
@@ -50,6 +61,29 @@ def get_student_data():
 
     resJson = {"sam": resArray, "hero": resArray, "lucky": resArray}
 
+    return resJson
+
+
+# 公共调用方法
+def user_data(model: StudentModel):
+
+    res = {"name": model.name,
+           "student_id": model.student_id,
+           "age": model.age,
+           "height": model.height,
+           "sex": model.sex,
+           "weight": model.weight}
+
+    resArray = [res, res, res, res, res]
+    return resArray
+
+
+# post 接收参数用户信息录入
+@app.post("/test/send_student_data/")
+def send_student_data(model: StudentModel):
+
+    resArray = user_data(model)
+    resJson = {"sam": resArray, "hero": resArray, "lucky": resArray}
     return resJson
 
 
